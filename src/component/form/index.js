4520307b6a289caf66948aa24595181e08c4f1cd
irfansettz <irfansettz@gmail.com>
2023-02-06 23:33:56 +0700

    ^validasi todo

--- src/component/form/index.js
+++ src/component/form/index.js
@@ -13,6 +13,10 @@ const Form = ({getTodo}) => {
     const [input, inputSet] = useState('');
     const submitHandler = (e) => {
         e.preventDefault();
+        if (input === '') {
+            alert('todo tidak boleh kosong');
+            return;
+        }
         let id = nanoid(8);
         let newTodo = input;
         let isDone = false;
@@ -26,7 +30,7 @@ const Form = ({getTodo}) => {
 
     return (
         <>
-            <form onSubmit={submitHandler}>
+            <form className="form-todo" onSubmit={submitHandler}>
                 <div className="form-control">
                     <input type="text" value={input} onChange={(e)=> inputSet(e.target.value)} className="input" placeholder="Enter your todo" autoComplete="off" />
                 </div>
